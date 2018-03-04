95a0c0c6e82e7251ab67782d479c34996567a3ea
Avsecz <zigaavsec@gmail.com>
2018-03-04 11:54:38 +0100

    set config to default [ci skip]

--- app/models/cache.py
+++ app/models/cache.py
@@ -4,7 +4,7 @@ from flask_caching import Cache
 from app import app
 
 
-cache = Cache(config={'CACHE_TYPE': 'memcached',
+cache = Cache(config={'CACHE_TYPE': app.config['CACHE_TYPE'],
                       'CACHE_DEFAULT_TIMEOUT': app.config['CACHE_TIMEOUT'],
                       'CACHE_KEY_PREFIX': 'kipoi_website',
                       'CACHE_MEMCACHED_SERVERS': app.config['MEMCACHED_SERVERS'],
